51ba2982d5f9114380515f8f13b300945b3c5035
cnorena <cnorena@sistecredito.com>
2024-08-25 20:24:23 -0500

    fix back component

--- src/1-presentation/Components/Drawflow/node/back.vue
+++ src/1-presentation/Components/Drawflow/node/back.vue
@@ -40,27 +40,6 @@ const namespace = storeDrawFlow.nodes.items[props.index].data.namespace;
         </DrawflowNodeConnection>
       </div>
     </div>
-      <template #default="{ item, index, open }">
-        <UButton color="gray" variant="ghost" class="border-b border-gray-200 dark:border-gray-700" :ui="{ rounded: 'rounded-none', padding: { sm: 'p-3' } }">
-          <template #leading>
-            <UIcon
-              name="i-heroicons-chevron-right-20-solid"
-              class="w-6 h-5 transform transition-transform duration-200"
-              :class="[open && 'rotate-90']"
-            />
-            
-          </template>
-
-          <span class="truncate">{{ index + 1 }}. {{ item.label }}</span>
-
-          <template #trailing>
-            <div class="w-6 h-6 ms-auto rounded-full bg-primary-500 dark:bg-primary-400 flex items-center justify-center -my-1">
-              <UIcon :name="item.icon" class="w-4 h-4 text-white dark:text-gray-900" />
-            </div>
-          </template>
-        </UButton>
-      </template>
-    </UAccordion> -->
   </div>
 </template>
 
